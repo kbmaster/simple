ef98c8b2d68a663549b8156c7d0bc80c7270e2cf
kbmaster <kbmaster231281@hotmail.com>
2018-09-18 11:33:36 -0300

    test service

--- tests/test-service.sh
+++ tests/test-service.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+
+echo "Test service!!"
+
 status=$(curl -w "%{http_code}\\n" -H "Accept:application/json" -H "Content-Type:application/json"  https://radiant-crag-93001.herokuapp.com/service.php  -s -o /dev/null)
 
-if [$status -neq 200]; then 
+echo "Status $status"
+
+if [ "$status" == "200" ]; then 
 echo "El servicio no responde\n"
 exit 1;
 fi
