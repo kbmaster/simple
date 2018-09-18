e84c7f70067f3d456cff618056435bbe9504cbf2
kbmaster <kbmaster231281@hotmail.com>
2018-09-18 10:12:52 -0300

    more tests

---
+++ service.php
@@ -0,0 +1,6 @@
+<php?
+
+//http_response_code(440);
+http_response_code(200);
+
+?>
---
+++ tests/test-service.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+status=$(curl -w "%{http_code}\\n" -H "Accept:application/json" -H "Content-Type:application/json"  https://radiant-crag-93001.herokuapp.com/service.php  -s -o /dev/null)
+
+if [$status -neq 200]; then 
+echo "El servicio no responde\n"
+exit(1);
+fi
+
+exit(0);
